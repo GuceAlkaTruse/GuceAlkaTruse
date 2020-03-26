d8fb3d12d32c20324ec0693a50ec62668d4a2fcf
GuceAlkaTruse <60811855+GuceAlkaTruse@users.noreply.github.com>
2020-03-26 17:33:06 +0200

    first commit

---
+++ New Unity Project (3)/Assets/Scripts/SignalingOff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalingOff : MonoBehaviour
+{
+    [SerializeField] private AudioSource _sourceOff;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BestRob"))
+        {
+        }
+    }
+}
